901b90ca6be2fa6aa5f3c1052b3fa2c4b20d6f4a
Rodrigo <rodrigocardosotadeo@gmail.com>
2025-08-29 11:24:00 -0300

    criando o componente Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,20 @@
-
+import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header>
+        <nav>
+            <img src="" alt="logo" />
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/cliente">Cliente</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
   )
 }
 
