fa82b29e15f5d35c848b48954eeef7fff509d838
Rodrigo <rodrigocardosotadeo@gmail.com>
2025-09-04 09:03:56 -0300

    estilizando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
-
+import {FaFacebook} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-900 text-white p-6 flex flex-col items-center justify-center space-y-4">
+      <h3>&copy; 2025 - Todos o direitos reservados</h3>
+      <div className="flex space-x-4">
+        <a>
+          <FaFacebook className="w-10 h-10 hover:text-blue-400"/>
+        </a>
+        <a>
+          <FaInstagram className="w-10 h-10 hover:text-blue-400"/>    
+        </a>   
+      </div>
+         
+    </footer>
   )
 }
 
